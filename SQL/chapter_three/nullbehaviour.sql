d96f04f4b6b411ac6ebbef9468db436639335947
pickleprat <prat16here@gmail.com>
2024-09-11 21:02:49 +0530

    added 'correlated subquery' theory

--- SQL/chapter_three/nullbehaviour.sql
+++ SQL/chapter_three/nullbehaviour.sql
@@ -32,7 +32,7 @@ SELECT main_id FROM source WHERE NOT (
 
 SELECT main_id FROM source WHERE NOT (
 	main_id = 20 OR -- 30 = 20 => False 
-	main_id = 50 OR -- 20 = 50 => False  
+	main_id = 50 OR -- 30 = 50 => False  
 	main_id = NULL -- always null => Null  
 ); 
 
@@ -42,5 +42,29 @@ SELECT main_id FROM source WHERE NOT (
 -- Null 
 -- which isn't true which means it won't select this element either 
 -- therefore it returns an empty set 
+
+-- to remedy this you can either use coalesce 
+SELECT main_id FROM source WHERE main_id NOT IN (
+	SELECT COALESCE(target_id, 0) FROM target 
+); 
+
+-- but this would create a fake 0 in the target_id column, which won't really be a problem unless there's a 0 in the main_id and 
+-- not in the target ids. You can always make sure that the target_id is being coalesced with a value that doesn't exist in the main_id 
+
+-- you could also do 
+SELECT main_id FROM source WHERE main_id NOT IN (
+	SELECT target_id FROM target WHERE target_id 
+	IS NOT NULL 
+); 
+
+-- but a more appropirate way to do it is as follows: 
+SELECT main_id FROM source src WHERE main_id NOT EXISTS (
+	SELECT target_id FROM target tgt WHERE 
+	src.main_id = tgt.target_id
+)
+-- this method is called a correlated subquery in which each value in the outer query 
+-- is iteratively referenced in the correlated subquery 
+
+
 
 
